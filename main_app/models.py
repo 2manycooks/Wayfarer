a85f12accec3b831d954de3cda76139669f23fea
Joma121 <martinsjoshua1986@gmail.com>
2021-01-14 16:55:30 -0800

    adds str dunder to models

--- main_app/models.py
+++ main_app/models.py
@@ -20,9 +20,15 @@ class Profile(models.Model):
     def save_user_profile(sender, instance, **kwargs):
         instance.profile.save()
 
+    def __str__(self):
+        return self.user.username
+
 class City(models.Model):
     name = models.CharField(max_length=100)
     image = models.ImageField(upload_to='city_image', blank=True)
+
+    def __str__(self):
+        return self.name
 
 class Post(models.Model):
     title = models.CharField(max_length=100)
@@ -32,6 +38,9 @@ class Post(models.Model):
     city = models.ForeignKey(City, on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
 
     class Meta:
         ordering = ['-created_at']
