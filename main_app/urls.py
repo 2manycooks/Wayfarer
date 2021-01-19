615387adb9a485df5235a5f30cc0c28193679b22
logankendall11 <logan_kendall11@yahoo.com>
2021-01-19 15:32:51 -0800

    m

--- main_app/urls.py
+++ main_app/urls.py
@@ -7,11 +7,8 @@ urlpatterns = [
     path('users/', views.users_profile, name='profile'),
     path('login_failure/', views.login_failure, name='login_failure'),
     path('account/signup', views.signup, name='signup'),
-<<<<<<< HEAD
     path('posts/<int:post_id>', views.show_post, name='show_post'),
-=======
     path('users/edit', views.users_edit, name='users_edit'),
     # path('users/delete', views.users_delete, name='users_delete'),
    
->>>>>>> d020890a64b4a016683928a237091dff449a595a
 ] 
