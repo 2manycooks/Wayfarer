e88562f0ab88b6c6eada2c6ee2736576b3274db7
kmylan <mylankate@gmail.com>
2021-01-20 22:57:49 -0800

    Edit Post stylized

--- wayfarer/urls.py
+++ wayfarer/urls.py
@@ -19,8 +19,8 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('admin', admin.site.urls),
+    path('admin/', admin.site.urls),
     path('', include('main_app.urls')),
-    path('', include('django.contrib.auth.urls')),
+    path('/', include('django.contrib.auth.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
